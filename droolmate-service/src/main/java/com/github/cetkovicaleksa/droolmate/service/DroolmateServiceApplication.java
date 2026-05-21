8b6f82a9f59fef1e0867197737a9a49f6ff608cc
Алекса Ћетковић <cetkovic.sv77.2022@uns.ac.rs>
2026-05-22 00:12:39 +0200

    init model, rules and service projects

---
+++ droolmate-service/src/main/java/com/github/cetkovicaleksa/droolmate/service/DroolmateServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.cetkovicaleksa.droolmate.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroolmateServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DroolmateServiceApplication.class, args);
+    }
+
+}
